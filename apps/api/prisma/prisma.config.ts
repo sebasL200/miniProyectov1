b34fdcdbc863a58e2009f6bd758f97030d9c4d2b
cono200 <tatattian04@gmail.com>
2026-05-15 10:19:45 -0700

    Ultimos cambios

--- apps/api/prisma/prisma.config.ts
+++ apps/api/prisma/prisma.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   schema: path.join(__dirname, 'schema.prisma'),
   datasource: {
-    url: process.env.DATABASE_URL || 'file:./dev.db',
+    // Si no encuentra el .env, usará esta cadena directamente
+    url: process.env.LOCAL_DB_URL || 'postgres://postgres:password@localhost:5435/ecommerce_catalogo_mini?sslmode=disable',
   },
 });
--- apps/api/src/app/prisma/prisma.service.ts
+++ apps/api/src/app/prisma/prisma.service.ts
@@ -1,16 +1,13 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import * as path from 'path';
 
 @Injectable()
 export class PrismaService implements OnModuleInit, OnModuleDestroy {
   private readonly prismaClient: PrismaClient;
 
   constructor() {
-    const dbPath = path.resolve(__dirname, '..', '..', '..', 'prisma', 'dev.db');
-    this.prismaClient = new PrismaClient({
-      datasourceUrl: process.env.DATABASE_URL || `file:${dbPath}`,
-    });
+    // Eliminamos la lógica de SQLite y le pasamos la URL de Postgres
+    this.prismaClient = new PrismaClient();
   }
 
   get client(): PrismaClient {
